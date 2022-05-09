dba78d04b80ac89859b6610c2691301b4a491292
Lorenzo Rubboli <lorenzo.rubboli2@studio.unibo.it>
2022-05-09 20:44:09 +0200

    Last commit

--- Rubboli/Zombie/Zombie.cs
+++ Rubboli/Zombie/Zombie.cs
@@ -11,44 +11,6 @@ public class Zombie : AbstractHealthEntity
     }
 
     public int Damage { get; }
-
-public class Builder
-    {
-        private Point2D spawnPoint;
-        private double speed;
-        private int maxHp;
-        private int damageDealt;
-
-        private Direction direction;
-
-        public Builder(Point2D spawnPoint)
-        {
-            this.spawnPoint = spawnPoint;
-        }
-
-        public Builder Speed(double speed)
-        {
-            this.speed = speed;
-            return this;
-        }
-
-        public Builder MaxHp(int maxHp)
-        {
-            this.maxHp = maxHp;
-            return this;
-        }
-
-        public Builder DamageDealt(int dmg)
-        {
-            this.damageDealt = dmg;
-            return this;
-        }
-
-        public Zombie build()
-        {
-            return new Zombie(spawnPoint, direction, speed, EntityType.ZOMBIE, maxHp, damageDealt);
-        }
-    }
 }
 
 
